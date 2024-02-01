f042e6ddb2f30d816341fe59f409a6b3029a10be
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-02-01 13:07:47 +0530

    [fix] Minor UI Change

--- src/components/StartingPage/StartingPageContent.js
+++ src/components/StartingPage/StartingPageContent.js
@@ -1,10 +1,22 @@
-import classes from './StartingPageContent.module.css';
+import { useContext } from "react";
+import classes from "./StartingPageContent.module.css";
+import AuthContext from "../../store/auth-context";
 
 const StartingPageContent = () => {
+  const authCtx = useContext(AuthContext);
   return (
-    <section className={classes.starting}>
-      <h1>Welcome on Board!</h1>
-    </section>
+    <div>
+      {authCtx.isLoggedIn && (
+        <section className={classes.starting}>
+          <h1>Welcome on Board!</h1>
+        </section>
+      )}
+      {!authCtx.isLoggedIn && (
+        <section className={classes.starting}>
+          <h1>Welcome to JWT Auth Space</h1>
+        </section>
+      )}
+    </div>
   );
 };
 
